1934f1d9b7d82671bb52c1482ff08a21e90e2943
skalback <70301513+skalback@users.noreply.github.com>
2021-02-15 22:17:44 -0500

    updated server for mongo atlas

--- server.js
+++ server.js
@@ -15,13 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(
+ process.env.MONGODB_URI || "mongodb://localhost/Budget-Tracker",
+  {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
-   
-});
+   }
+);
 
 // routes
 app.use(require("./routes/api.js"));
